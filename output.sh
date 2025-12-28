aa41b9dd8167b6e40075a0be9080ffd303dcc593
Tim Lamborn <tlamborn@gmail.com>
2025-12-28 20:50:27 +1300

    Move project into subdir

--- output.sh
+++
@@ -1,2 +0,0 @@
-#! /usr/bin/bash
-python3 "scripts/output.py" -l 4 --format jlc --gif --gif-duration 5.0
---
+++ puzzlebox-main/output.sh
@@ -0,0 +1,2 @@
+#! /usr/bin/bash
+python3 "../scripts/output.py" -l 4 --format jlc --gif --gif-duration 5.0
